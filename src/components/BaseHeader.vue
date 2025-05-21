ea3d2464daf47925faefc6f74f67093e4324a978
Geoxor <34042825+Geoxor@users.noreply.github.com>
2025-05-21 20:46:09 +0300

    align header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,13 +1,21 @@
 <template>
   <header class="absolute-x text-14px z-10 items-center flex justify-center w-full py-8 gap-6">
-    <button class="text-[#08080B] text-14px font-semibold bg-[#A68CDA] rounded-full py-2 px-4 cursor-pointer "  >
+
+    <div class="w-full flex justify-end">
+      <button class="text-[#08080B] text-14px font-semibold bg-[#A68CDA] rounded-full py-2 px-4 cursor-pointer "  >
       <a class='text-inherit no-underline' target="_blank" href="https://amethyst.geoxor.moe/guides/installation#installing-precompiled-packages">Download</a>
-    </button>
+      </button>
+    </div>
+
     <button>
       <AmethystLogo />
     </button>
-    <a class="text-[#E1D1FE] text-opacity-75 hover:text-opacity-100" target="_blank" href="https://github.com/Geoxor/amethyst">GitHub</a>
-    <a class="text-[#E1D1FE] text-opacity-75 hover:text-opacity-100" target="_blank" href="https://amethyst.geoxor.moe/user-manual">Documentation</a>
+
+    <div class="w-full flex gap-2">
+      <a class="text-[#E1D1FE] text-opacity-75 hover:text-opacity-100" target="_blank" href="https://github.com/Geoxor/amethyst">GitHub</a>
+      <a class="text-[#E1D1FE] text-opacity-75 hover:text-opacity-100" target="_blank" href="https://amethyst.geoxor.moe/user-manual">Documentation</a>
+    </div>
+
   </header>
 </template>
 
